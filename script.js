09d71b86fce0b241a82e208cd436b3f6043d9970
virshyla <142292589+virshyla@users.noreply.github.com>
2024-03-03 15:30:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+window.onload = function() {
+    let prel = document.querySelector('.prel')
+    prel.classList.add('hidePrel')
+    setTimeout(function() {
+        prel.classList.add('prelHiden')
+    },950)
+}
+
+let img = document.querySelectorAll('img')
+let imgInt
+
